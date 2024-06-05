f9380c4cc0bf3f14dc4f6f19fec021adfeecc4f5
Bora Uysal <163774229+borauysl@users.noreply.github.com>
2024-06-05 22:54:02 +0300

    Update Project.cs

--- veriyonetim/Controllers/Project.cs
+++ veriyonetim/Controllers/Project.cs
@@ -1,18 +1,37 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using MySql.Data.MySqlClient;
 
 public class Project
 {
     [Required]
+    [DataType(DataType.Date)]
+    public DateTime ProjeOneriTarihi { get; set; }
+
+    [Required]
     public string ProjeAdi { get; set; }
 
     [Required]
-    public string ProjeYapimcisi { get; set; }
+    public string ProjeTuru { get; set; }
 
     [Required]
-    [DataType(DataType.Date)]
-    public DateTime ProjeTarihi { get; set; }
+    public string ProjeDanismani { get; set; }
+
+    [Required]
+    public string ProjeYurutucusu { get; set; }
+
+    [Required]
+    public string ProjeEkibi { get; set; }
+
+    [Required]
+    public string ProjeKonusu { get; set; }
+
+    [Required]
+    public string ProjeAmaci { get; set; }
+
+    [Required]
+    public string ProjeHedefKitlesi { get; set; }
 
     [Required]
     [Range(1, int.MaxValue, ErrorMessage = "Süre 1 günden fazla olmalıdır.")]
@@ -22,5 +41,28 @@ public class Project
     [Range(0, int.MaxValue, ErrorMessage = "Bütçe negatif olamaz.")]
     public int ProjeButcesi { get; set; }
 
-  
-}
+    [Required]
+    public string ProjeEtkilikleri { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime ProjeBaslangicTarihi { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime ProjeBitisTarihi { get; set; }
+
+    [Required]
+    public string ProjeKurumKuruluslar { get; set; }
+
+    [Required]
+    public string ProjeMateryaller { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (ProjeBitisTarihi < ProjeBaslangicTarihi)
+        {
+            yield return new ValidationResult("Proje bitiş tarihi, başlangıç tarihinden küçük olamaz.", new[] { "ProjeBitisTarihi" });
+        }
+    }
+  }
